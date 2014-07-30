5ca682269098a73f070ff5bd5773624ec41aac33
Leo Rossignac-Milon and Rob Dimsdale <lrossignacmilon@pivotallabs.com>
2014-07-30 15:42:24 -0700

    Add MySQL job to purge old GRA_*.log
    [#74890134]

---
+++ src/gra-log-purger/README.md
@@ -0,0 +1,1 @@
+This directory contains a go process to manage the purging of GRA log files for MySQL. If left unchecked, these log files can grow unbounded.
---
+++ src/gra-log-purger/src/gra-log-purger/runner.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"time"
+)
+
+var graLogDir = flag.String(
+	"graLogDir",
+	"",
+	"Specifies the directory from which to purge GRA log files.",
+)
+
+var graLogDaysToKeep = flag.Int(
+	"graLogDaysToKeep",
+	30,
+	"Specifies the maximum age of the GRA log files allowed.",
+)
+
+func main() {
+	flag.Parse()
+	for {
+		out, err := runCommand("sh", "/var/vcap/jobs/mysql/bin/gra-log-purger.sh", *graLogDir, strconv.Itoa(*graLogDaysToKeep))
+		if err != nil {
+			LogErrorWithTimestamp(err)
+		}
+		LogWithTimestamp(out)
+		LogWithTimestamp("Sleeping for one hour\n")
+		time.Sleep(1 * time.Hour)
+	}
+}
+
+// Runs command with stdout and stderr pipes connected to process
+func runCommand(executable string, args ...string) (string, error) {
+	cmd := exec.Command(executable, args...)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return string(out), err
+	}
+	return string(out), nil
+}
+
+func LogWithTimestamp(format string, args ...interface{}) {
+	fmt.Printf("[%s] - ", time.Now().Local())
+	if nil == args {
+		fmt.Printf(format)
+	} else {
+		fmt.Printf(format, args...)
+	}
+}
+
+func LogErrorWithTimestamp(err error) {
+	fmt.Fprintf(os.Stderr, "[%s] - ", time.Now().Local())
+	fmt.Fprintf(os.Stderr, err.Error() + "\n")
+}
