f78967ba30660fab51301ce345665df5cf12e40b
Nitya Dhanushkodi <ndhanushkodi@pivotal.io>
2018-04-26 14:53:28 -0700

    Fix drain script to actually stop mysql [#157135700]
    Signed-off-by: Joseph Palermo <jpalermo@pivotal.io>

--- jobs/mysql/templates/drain.sh
+++ jobs/mysql/templates/drain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-/var/vcap/packages/mariadb/support-files/mysql.server stop > /dev/null
+/var/vcap/packages/mariadb/support-files/mysql.server stop --pid-file=/var/vcap/sys/run/mysql/mysql.pid > /dev/null
 return_code=$?
 echo 0
 exit ${return_code}
