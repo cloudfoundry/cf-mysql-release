aa04a97640ccd1bad4a070868fb4b8237b0d73f4
Joseph Palermo <jpalermo@pivotal.io>
2018-05-10 15:32:36 -0700

    Prevent drain script from failing if release is disabled [#157496349]

--- jobs/mysql/templates/drain.sh
+++ jobs/mysql/templates/drain.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -e
 
+<% if p('cf_mysql_enabled') == true %>
 /var/vcap/packages/mariadb/support-files/mysql.server stop --pid-file=/var/vcap/sys/run/mysql/mysql.pid > /dev/null
 return_code=$?
 echo 0
 exit ${return_code}
+<% else %>
+echo 0
+exit 0
+<% end %>
