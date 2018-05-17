edf33cb0ec9c14e60a8f8714f26d9e9e6e07dce6
Carlo Alberto Ferraris <github@cafxx.strayorange.com>
2018-05-17 11:34:54 +0900

    Do not stop the node in drain That's what `monit stop` is supposed to do

--- jobs/mysql/templates/drain.sh
+++ jobs/mysql/templates/drain.sh
@@ -55,17 +55,5 @@ for NODE in "${CLUSTER_NODES[@]}"; do
   fi
 done
 
-<% if p('cf_mysql_enabled') == true %>
-PIDFILE=/var/vcap/sys/run/mariadb_ctl/mariadb_ctl.pid
-source /var/vcap/packages/cf-mysql-common/pid_utils.sh
-
-set +e
-kill_and_wait ${PIDFILE} 300 0 > /dev/null
-return_code=$?
-
-echo 0
-exit ${return_code}
-<% else %>
-echo 0
-exit 0
-<% end %>
+echo "Drain Success" &>> "$LOG_DIR/drain.log"
+echo 0; exit 0 # drain success
