38f317d9560193e3ecc895a0ac484cd6ae1ade86
Nitya Dhanushkodi <ndhanushkodi@pivotal.io>
2018-07-16 09:58:27 -0700

    Use kill_and_wait for stopping mysql; mysqladmin does not work when node is non-primary [#157299144]
    Signed-off-by: Joseph Palermo <jpalermo@pivotal.io>

--- jobs/mysql/templates/drain.sh
+++ jobs/mysql/templates/drain.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -e
 
 <% if p('cf_mysql_enabled') == true %>
-/var/vcap/packages/mariadb/support-files/mysql.server stop --pid-file=/var/vcap/sys/run/mysql/mysql.pid > /dev/null
+PIDFILE=/var/vcap/sys/run/mariadb_ctl/mariadb_ctl.pid
+source /var/vcap/packages/cf-mysql-common/pid_utils.sh
+
+set +e
+kill_and_wait ${PIDFILE} 300 0 > /dev/null
 return_code=$?
+
 echo 0
 exit ${return_code}
 <% else %>
